b03f4b3669959b8d33060032cf70c01872dc051e
Vinicius de Andrade Sampaio <cobracaptali@gmail.com>
2025-05-13 18:01:29 -0300

    Add files via upload
    a mesma atividade porem atualizando pois esqueci de utilizar o while e o  do while e so usei o for

---
+++ Atividade 12.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+
+int main (){
+int escolha,escolha2,escolha3,escolha4;
+printf("\n\n- Xadrez -\n\n");
+printf("1.Jogar\n");    
+printf("2.Sair\n");
+scanf("%d", &escolha);
+switch (escolha)
+{
+case 1:
+    printf("Qual peça Gostaria de Mover\n");
+    printf("1.Rainha\n");
+    printf("2.Bispo\n");
+    printf("3.Torre\n");
+    scanf("%d" ,&escolha2);
+     if (escolha2 == 1)
+     {
+        printf("1 Diagonal\n");
+        printf("2.Lados\n");
+        scanf("%d" , &escolha3);            
+        if (escolha3 == 1)
+        {
+            printf("1.Esquerda \n2.Direita\n");
+            scanf("%d" ,&escolha4);
+            if (escolha4 == 1)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    printf("Moveu a Rainha Para Diagonal Esquerda (%d)\n", i);
+                }
+                
+            }
+            else if (escolha4 == 1)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    printf("Moveu a Rainha Para Diagonal Direita (%d)\n", i);
+                }
+            }
+            
+        }
+        else if (escolha3 == 2)
+        {
+            printf("1.Esquerdo\n2.Direito\n");
+            scanf("%d", &escolha4);
+            if (escolha4 == 1)
+            {
+                for (int i = 1; i < 6; i++)
+                {
+                    printf("Moveu a Rainha Para lado Esquerdo (%d)\n", i);
+                }
+                
+            }
+            else if (escolha4 == 2)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    printf("Moveu a Rainha Para lado direito (%d)\n", i);
+                }
+                
+            }
+            else
+             {
+             printf("Opçao Invalida\n");
+             }
+        }
+        else
+        {
+            printf("Opçao Invalida\n");
+        }
+     }
+    else if (escolha2 == 2)
+    {
+        printf("Diagonal\n");
+        printf("1.Esquerda\n2.Direita\n");
+        scanf("%d" ,&escolha3);
+        if (escolha3 == 1)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                printf("Voce Moveu o Bispo Para Diagonal Esquerda (%d)\n", i);
+            }
+            
+        }
+        else if (escolha3 == 2)
+        {   
+            for (int i = 1; i < 6; i++)
+            {
+                 printf("Voce Moveu o Bispo Para Diagonal Direita (%d)\n", i);
+            }
+        }
+
+        else
+        {
+            printf("Opçao Invalida\n");
+        }
+    }
+    else if (escolha2 == 3)
+    {
+        printf("1.Direita\n2.Esquerda\n");
+        scanf("%d",&escolha3);
+        if (escolha3 == 1)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+            printf("Voce Moveu a Torre para Direita (%d)\n",i);
+            }
+        }
+        else if (escolha3 == 2)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+            printf("Voce Moveu a Torre para Esquerda (%d)\n",i);
+            }
+        }
+        else
+        {
+            printf("Opçao Invalida");
+        }
+    }
+    else
+    {
+        printf("Opçao Invalida");
+    }
+
+    break;
+default:
+    break;
+}
+}
