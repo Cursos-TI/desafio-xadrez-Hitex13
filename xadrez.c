a5dff18da6fc170ab545e99d2385dc1f17015c30
Vinicius de Andrade Sampaio <cobracaptali@gmail.com>
2025-05-21 00:35:53 +0000

    Nivel mestre Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,20 +1,76 @@
 #include <stdio.h>
 
-int main (){
-
-for (int i = 1; i <= 1; i++)
+void torre(){
+for (int T = 1; T <= 5; T++)
 {
-    int j;
-    for (int j = 1; i <= 2; i++)
-    {
-       printf("Baixo\n");
-    }
-    printf("lado\n");
+    printf("Torre moveu para direita(%d)\n",T);
+}
 
+}
+
+void bispo(){
+for (int l = 0; l < 5; l++)
+{
+    int k;
+    for (int k = 1; k <= 5; k++)
+    {
+       printf("Bispo moveu para diagonal direita (%d)\n", k);
+    }
+    if (k = 5)break;
+    
+    
+}
+}
+void cavalo(){
+
+for (int i  = 0, j = 0, C = 1; i <= 2; i++ , j++ ,C++)
+{
+
+    if (i < 2){
+    printf("cavalo moveu para cima(%d)\n" , i);}
+    if (j == 2)
+    printf("cavalo moveu para direita(1)\n");
+
+}
+}
+void Rainha(){
+
+for (int R = 1; R <= 8; R++)
+{
+    printf("Rainha Moveu uma casa para esquerda(%d)\n",   R);
 }
 
 
 
+}
 
+int main(){
+
+int escolha;
+printf("Inciando...\nQual peça gostaria de mover\n");
+printf("1.Torre\n2.Bispo\n3.Rainha\n4.Cavalo\n");
+scanf("%d" , &escolha);
+
+switch (escolha)
+{
+case 1:
+    torre();
+    break;
+case 2:
+    bispo();
+    break;
+case 3:
+    Rainha();
+    break;
+case 4:
+    cavalo();
+    break;
+default:
+printf("Opçao Errada");
+    break;
+}
+
+
+return 0;
 
 }
